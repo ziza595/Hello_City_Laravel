b925b861d2a00c300d6acff600a14edef7265651
abdoulaziz <ziza595@gmail.com>
2020-12-31 23:29:21 +0000

    Add styling with Tailwind CSS

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -3,9 +3,14 @@
 @section('title', 'About Us | '.config('app.name'))
 
 @section('content')
-    <img src="{{ asset('/img/default.png') }}" alt="User logo">
+    <img src="{{ asset('/img/default.png') }}" alt="User logo" class="my-12 rounded-full shadow-md">
 
-    <p>Built with &hearts; by Abdoul Aziz.</p>
+    <h2 class="mb-5 text-gray-700">
+        Built with <span class="text-pink-500">&hearts;</span> by Abdoul Aziz.
+    </h2>
 
-    <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
+    <p>
+        <a href="{{ route('home') }}" class="text-indigo-500
+        hover:text-indigo-600 underline">Revenir à la page d'accueil</a>
+    </p>
 @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,9 +3,9 @@
 @section('title', config('app.name'))
 
 @section('content')
-    <img src="{{ asset('/img/senegal.jpeg') }}" alt="Sénégal Flag">
+    <img src="{{ asset('/img/senegal.jpeg') }}" alt="Senegal Flag" class="mt-12 rounded shadow-md">
 
-    <h1>Hello from Quebec !</h1>
+    <h1 class="mt-5 text-3xl sm:text-5xl text-semibold text-indigo-600">Hello from Sénégal !</h1>
 
-    <p>It's currently {{ date('G:i') }}.</p>
+    <p class="text-lg text-gray-800">Il est actuellement {{ date('G:i') }}.</p>
 @endsection
